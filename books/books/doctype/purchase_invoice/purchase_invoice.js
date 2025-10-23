f5e39e58f29508eaa183e4941432656476358a6e
Praveenkumar26-S <kumarpraveen97917@gmail.com>
2025-10-23 15:46:07 +0530

    purchase_return

--- books/books/doctype/purchase_invoice/purchase_invoice.js
+++ books/books/doctype/purchase_invoice/purchase_invoice.js
@@ -1,11 +1,9 @@
-// Copyright (c) 2025, Praveen and contributors
+// Copyright (c) 2025, Praveen
 // For license information, please see license.txt
-
 
 frappe.ui.form.on("Purchase Invoice", {
     refresh(frm) {
         if (!frm.is_new() && frm.doc.docstatus === 1) {
-
             frm.add_custom_button(__('Create Payment Entry'), function() {
                 frappe.call({
                     method: "books.books.doctype.purchase_invoice.purchase_invoice.create_payment_entry",
@@ -17,57 +15,62 @@ frappe.ui.form.on("Purchase Invoice", {
                     }
                 });
             }, __("Create"));
-
             frm.add_custom_button(__('Return Items'), function() {
                 let dialog = new frappe.ui.Dialog({
                     title: __('Return Purchase Items'),
                     size: 'large',
                     fields: [
                         {
-                            fieldname: 'purchase_items',
+                            fieldname: 'return_items',
                             fieldtype: 'Table',
                             label: 'Items to Return',
-                            cannot_add_rows: true,
+                            cannot_add_rows: false,
                             in_place_edit: true,
                             fields: [
-                                { fieldname: 'item', fieldtype: 'Data', label: 'Item', read_only: 1, columns: 2 },
-                                { fieldname: 'description', fieldtype: 'Data', label: 'Description', read_only: 1, columns: 3 },
-                                { fieldname: 'uom', fieldtype: 'Data', label: 'UOM', read_only: 1, columns: 1 },
-                                { fieldname: 'max_qty', fieldtype: 'Float', label: 'Purchased Qty', read_only: 1, columns: 1 },
-                                { fieldname: 'return_qty', fieldtype: 'Float', label: 'Return Qty', reqd: 1, columns: 1 },
-                                { fieldname: 'rate', fieldtype: 'Currency', label: 'Rate', read_only: 1, columns: 1 },
-                                { fieldname: 'amount', fieldtype: 'Currency', label: 'Amount', read_only: 1, columns: 1 }
+                                { fieldname: 'item', fieldtype: 'Link', options: 'Item', label: 'Item', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'uom', fieldtype: 'Link', options: 'UOM', label: 'UOM', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'qty', fieldtype: 'Float', label: 'Return Qty', reqd: 1, in_list_view: 1 },
+                                { fieldname: 'rate', fieldtype: 'Currency', label: 'Rate', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'amount', fieldtype: 'Currency', label: 'Amount', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'discount_percentage', fieldtype: 'Float', label: 'Discount %', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'discount_amount', fieldtype: 'Currency', label: 'Discount Amount', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'net_amount', fieldtype: 'Currency', label: 'Net Amount', read_only: 1, in_list_view: 1 },
+                                { fieldname: 'warehouse', fieldtype: 'Link', options: 'Warehouse', label: 'Warehouse', read_only: 1, in_list_view: 1 }
                             ],
                             data: (frm.doc.items || []).map(i => ({
                                 item: i.item,
-                                description: i.description || '',
                                 uom: i.uom,
-                                max_qty: i.qty,
-                                return_qty: 0,
+                                qty: i.qty,
                                 rate: i.rate,
-                                amount: 0
+                                amount: i.amount,
+                                discount_percentage: i.discount_percentage,
+                                discount_amount: i.discount_amount,
+                                net_amount: i.net_amount,
+                                warehouse: i.warehouse
                             }))
                         }
                     ],
                     primary_action_label: __('Create Return'),
                     primary_action(values) {
                         let return_items = [];
-
-                        (values.purchase_items || []).forEach(row => {
-                            if (row.return_qty > 0) {
-                                if (row.return_qty > row.max_qty) {
-                                    frappe.throw(`Return qty for ${row.item} cannot exceed purchased qty (${row.max_qty})`);
-                                }
-                                row.amount = row.return_qty * row.rate;
-                                return_items.push(row);
+                        (values.return_items || []).forEach(row => {
+                            if (row.qty > 0) {
+                                return_items.push({
+                                    item: row.item,
+                                    qty: row.qty,
+                                    rate: row.rate,
+                                    amount: row.amount,
+                                    discount_percentage: row.discount_percentage,
+                                    discount_amount: row.discount_amount,
+                                    net_amount: row.net_amount,
+                                    warehouse: row.warehouse
+                                });
                             }
                         });
-
                         if (!return_items.length) {
-                            frappe.msgprint(__('Please enter at least one item with a Return Qty greater than 0.'));
+                            frappe.msgprint(__('Please enter at least one item with a valid Return Qty.'));
                             return;
                         }
-
                         frappe.call({
                             method: "books.books.doctype.purchase_invoice.purchase_invoice.create_purchase_return",
                             args: {
@@ -76,16 +79,14 @@ frappe.ui.form.on("Purchase Invoice", {
                             },
                             callback: function(r) {
                                 if (r.message) {
-                                    frappe.msgprint(__('Purchase Return {0} created', [r.message]));
+                                    frappe.msgprint(__('Purchase Return {0} created successfully.', [r.message]));
                                     frappe.set_route('Form', 'Purchase Invoice', r.message);
                                 }
                             }
                         });
-
                         dialog.hide();
                     }
                 });
-
                 dialog.show();
             }, __("Create"));
         }
--- books/books/doctype/purchase_invoice/purchase_invoice.py
+++ books/books/doctype/purchase_invoice/purchase_invoice.py
@@ -12,66 +12,109 @@ class PurchaseInvoice(Document):
         for item in self.items:
             if not item.rate:
                 rate = frappe.db.get_value(
-					"Price List",
-					{"item":item.item, "uom": item.uom, "price_list_type": "Purchase", "name": self.price_list},
-					"rate"
-				)
+                    "Price List",
+                    {
+                        "item": item.item,
+                        "uom": item.uom,
+                        "price_list_type": "Purchase",
+                        "name": self.price_list,
+                    },
+                    "rate",
+                )
                 item.rate = rate or 0
+
             item.amount = (item.qty or 0) * (item.rate or 0)
             item.discount_amount = (item.amount * (item.discount_percentage or 0)) / 100
             item.net_amount = item.amount - item.discount_amount
+
             total_qty += item.qty or 0
             total += item.net_amount or 0
+
         self.total_qty = total_qty
         self.total_amount = total
-        
+
         if self.discount_type == "Percentage":
             self.discount_amount = (total * (self.discount_value or 0)) / 100
         elif self.discount_type == "Amount":
             self.discount_amount = self.discount_value or 0
         else:
             self.discount_amount = 0
-        
+
         self.grand_total = total - self.discount_amount
-        self.rounded_total = round(self.grand_total,2)
-     
+        self.rounded_total = round(self.grand_total, 2)
+
+
 @frappe.whitelist()
 def create_payment_entry(invoice_name):
     invoice = frappe.get_doc("Purchase Invoice", invoice_name)
+
+    total_paid = frappe.db.sql("""
+        SELECT SUM(amount_paid)
+        FROM `tabPayment Entry`
+        WHERE reference_invoice = 'Purchase Invoice' AND reference_name = %s AND docstatus = 1
+    """, (invoice.name,), as_dict=True)[0].get("SUM(amount_paid)") or 0
+
+    outstanding = invoice.rounded_total - total_paid
+    if outstanding <= 0:
+        frappe.throw("This invoice is already fully paid.")
+
     payment = frappe.new_doc("Payment Entry")
     payment.payment_type = "Pay"
     payment.party_type = "Supplier"
     payment.party = invoice.supplier
     payment.reference_invoice = "Purchase Invoice"
     payment.reference_name = invoice.name
-    payment.amount_paid = invoice.outstanding_amount
+    payment.amount_paid = outstanding
     payment.reference_amount = invoice.rounded_total
     payment.mode_of_payment = "Cash"
     payment.posting_date = frappe.utils.nowdate()
     payment.insert()
+
+    total_paid += payment.amount_paid
+    remaining_outstanding = invoice.rounded_total - total_paid
+    invoice.db_set("outstanding_amount", remaining_outstanding)
     invoice.db_set("payment_entry", payment.name)
+
+    frappe.msgprint(f"Payment Entry <b>{payment.name}</b> created successfully.")
     return payment.name
+
 
 @frappe.whitelist()
 def create_purchase_return(invoice_name, return_items):
-    return_items = frappe.parse_json(return_items)
-    pi = frappe.get_doc("Purchase Invoice", invoice_name)
+    import json
+    if isinstance(return_items, str):
+        return_items = json.loads(return_items)
+    invoice = frappe.get_doc("Purchase Invoice", invoice_name)
 
-    new_pi = frappe.new_doc("Purchase Invoice")
-    new_pi.is_return = 1
-    new_pi.return_against = pi.name
-    new_pi.supplier = pi.supplier
-    new_pi.company = pi.company
-    new_pi.items = []
+    return_invoice = frappe.new_doc("Purchase Invoice")
+    return_invoice.is_return = 1
+    return_invoice.return_against = invoice.name
+    return_invoice.supplier = invoice.supplier
+    return_invoice.company = invoice.company
+    return_invoice.posting_date = frappe.utils.nowdate()
+    return_invoice.price_list = invoice.price_list
+    return_invoice.discount_type = invoice.discount_type
+    return_invoice.discount_value = invoice.discount_value
 
     for i in return_items:
-        new_pi.append("items", {
+        return_invoice.append("items", {
             "item": i["item"],
+            "uom": i.get("uom"),
             "qty": -1 * float(i["qty"]),
-            "rate": i["rate"],
-            "amount": -1 * float(i["amount"]),
+            "rate": float(i.get("rate", 0)),
+            "amount": -1 * float(i.get("amount", 0)),
+            "discount_percentage": i.get("discount_percentage") or 0,
+            "discount_amount": -1 * float(i.get("discount_amount", 0) or 0),
+            "net_amount": -1 * float(i.get("net_amount", 0) or 0),
+            "warehouse": i.get("warehouse")
         })
 
-    new_pi.insert(ignore_permissions=True)
-    new_pi.submit()
-    return new_pi.name
+    if hasattr(return_invoice, "calculate_totals"):
+        return_invoice.calculate_totals()
+
+    return_invoice.insert(ignore_permissions=True)
+    return_invoice.submit()
+    return_invoice.db_set("status", "Returned")
+    
+    frappe.msgprint(f"Purchase Return <b>{return_invoice.name}</b> created successfully.")
+    return return_invoice.name
